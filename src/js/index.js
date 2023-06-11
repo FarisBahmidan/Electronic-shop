da2d117a7ec4920314e3c1b9d321b6887efd33fb
FarisBahmidan <farisbahmidan@gmail.com>
2023-06-12 00:05:34 +0300

    web

--- src/js/index.js
+++ src/js/index.js
@@ -121,3 +121,23 @@ function removeAllActive() {
         bullets.classList.remove("active");
     });
 }
+
+
+
+
+
+
+
+
+const starRatingForm = document.querySelector(".star-rating")
+const handleFormChange = (e) => {
+    console.log(e.target.value)
+    return e.target.value
+}
+
+starRatingForm.addEventListener("change", handleFormChange)
+
+
+
+
+
